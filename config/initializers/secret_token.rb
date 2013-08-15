0468e3af864d0091deb2efab3f8aa70cb7d75769
danecjensen <danecjensen@gmail.com>
2013-08-15 13:55:05 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wanderlust::Application.config.secret_token = '2674e8aa7c9509ab81c1ccd387b44cc53179ca5250c5627fcee77ac8692f008669523f457866ce802fc7fea0350a89d4489919d709c378158d6e1620c81d1053'
